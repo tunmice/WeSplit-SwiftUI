f36dccbca705c15fb09edec8a5a180f679f6cd1c
tunmice <t.otegbayo@gmail.com>
2021-06-30 16:18:01 +0100

    * understanding navigation views

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,25 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Form {
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
+        NavigationView {
+            Form {
+               
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                
             }
             
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-           
+            .navigationTitle("We Move").navigationBarTitleDisplayMode(.inline)
             
         }
+        
         
     }
 }
