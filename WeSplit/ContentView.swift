2d8588801527709fe2a083e81af2171b0b1381a4
tunmice <t.otegbayo@gmail.com>
2021-06-30 15:28:51 +0100

    *understanding section to visually split groups in a from

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         
         Form {
-            Group {
+            Section {
                 Text("Hello, world!")
                 Text("Hello, world!")
                 Text("Hello, world!")
@@ -19,7 +19,7 @@ struct ContentView: View {
                 Text("Hello, world!")
             }
             
-            Group {
+            Section {
                 Text("Hello, world!")
                 Text("Hello, world!")
                 Text("Hello, world!")
